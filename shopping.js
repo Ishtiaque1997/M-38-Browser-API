0b9300b738535c7ca5bdeece1555b622c29bff2e
Ishtiaque1997 <ishtiaquea695@gmail.com>
2021-09-22 12:33:06 +0600

    almost done

--- shopping.js
+++ shopping.js
@@ -1,14 +1,28 @@
+const displayLocalStorageCart=()=>{
+ const cart=getCart();
+ for (const name in cart){
+   displayProduct(name);
+ }
+}
+
 const addItem=()=>{
+
+ //reading the input value
  const nameField=document.getElementById('product-name');
  const name=nameField.value;
+ if(!name){
+   return ;
+ }
 
  //display in the ui
  displayProduct(name)
+
  //add to local storage
  addProductToCart(name);
 
  nameField.value='';
 }
+
 const displayProduct=name=>{
  const ul=document.getElementById('products');
  const li=document.createElement('li');
@@ -16,6 +30,7 @@ const displayProduct=name=>{
  ul.appendChild(li);
 }
 
+//function to get cart
 const getCart=()=>{
  const cart=localStorage.getItem('cart')
  let cartObj;
@@ -27,9 +42,20 @@ const getCart=()=>{
  }
  return cartObj;
 }
+
 const addProductToCart=name=>{
  const cart=getCart();
- cart[name]=1;
- const artStringified=JSON.stringify(cart);
- localStorage.setItem('cart',artStringified)
+ if(cart[name]){
+   cart[name]=cart[name]+1;
+ }
+ else{
+   cart[name]=1;
+ }
+ const cartStringified=JSON.stringify(cart);
+ localStorage.setItem('cart',cartStringified)
 }
+const placeOrder=()=>{
+  document.getElementById('products').textContent='';
+  localStorage.removeItem('cart')
+}
+displayLocalStorageCart();
